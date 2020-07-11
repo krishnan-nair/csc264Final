013d7a2e05ed946fe0e96c50f4308edef65b78f2
Krishnan Nair <krishnannair12@gmail.com>
2020-07-10 23:40:22 -0400

    added js

---
+++ scripts/signup.js
@@ -0,0 +1,29 @@
+// signup
+const form = document.querySelector('#sign-up');
+
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = form['email'].value;
+    const password = form['password'].value;
+    
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email,password).then(cred =>{
+        console.log(cred)
+        db.collection('users').add({
+            firstName: form.first-name.value,
+            lastName: form.last-name.value,
+            major: form.major.value,
+            year: form.year.value,
+            username: form.username.value,
+            email: form.email.value,
+        });
+        alert('You have successfully joined our team!');
+        open('viewQuestions.html',"_self")
+    }).catch(function(error){
+        alert(error.message);
+    })
+
+})
